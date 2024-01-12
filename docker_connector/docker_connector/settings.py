c6ec154ec283dc97c31a0db9506094f6259ff855
Alberto Hernández <albertiae.digital@gmail.com>
2024-01-12 13:57:48 -0400

    Updated DB connection to postgresql

--- docker_connector/docker_connector/settings.py
+++ docker_connector/docker_connector/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'docker_connector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': postgres_host,
+        'NAME': postgres_db,
+        'USER': postgres_user,
+        'PASSWORD': postgres_password,
     }
 }
 
